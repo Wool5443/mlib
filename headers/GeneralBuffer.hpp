2d3d24725dcc27c5419239aa94cd012b89acee6f
Wool5443 <mihsolodilov2015@gmail.com>
2024-07-08 19:58:31 +0300

    Replaced StringBuffer with GeneralBuffer

---
+++ headers/GeneralBuffer.hpp
@@ -0,0 +1,143 @@
+#ifndef MLIB_GENERAL_BUFFER
+#define MLIB_GENERAL_BUFFER
+
+#include <iostream>
+#include <cstring>
+#include "Utils.hpp"
+
+namespace mlib {
+
+template<typename T, size_t DefaultCapacity, size_t GrowFactor>
+class Buffer
+{
+protected:
+    char*  m_buf;
+    size_t m_capacity;
+public:
+    char*        RawPtr()       noexcept { return m_buf; }
+    const char*  RawPtr() const noexcept { return m_buf; }
+    size_t       Length;
+    Utils::Error Error = Utils::Error();
+private:
+    Buffer(size_t capacity, size_t length, const char* buf)
+        : m_buf(new T[capacity]{}), m_capacity(capacity), Length(length)
+    {
+        HardAssert(length < capacity, Utils::ErrorCode::ERROR_BAD_NUMBER);
+
+        if (!m_buf)
+        {
+            Error = CREATE_ERROR(Utils::ErrorCode::ERROR_NO_MEMORY);
+            return;
+        }
+        if (buf)
+            std::memcpy(m_buf, buf, length);
+    }
+public:
+    explicit Buffer(const char* buf = nullptr)
+        : Buffer(DefaultCapacity, 0, buf) {}
+
+    explicit Buffer(size_t hintLength, const char* buf = nullptr)
+        : Buffer(Buffer::calculateCapacity(hintLength), hintLength, buf) {}
+public:
+    Buffer(const Buffer& other)
+        : Buffer(other.m_capacity, other.Length, other.m_buf) {}
+
+    Buffer(Buffer&& other) noexcept
+        : m_buf(other.m_buf), m_capacity(other.m_capacity),
+          Length(other.Length)
+    {
+        other.m_buf = nullptr;
+    }
+
+    Buffer& operator=(const Buffer& other)
+    {
+        if (this == &other) return *this;
+
+        T* newBuf = new T[other.m_capacity]{};
+
+        if (!newBuf)
+        {
+            Error = CREATE_ERROR(Utils::ErrorCode::ERROR_NO_MEMORY);
+            return *this;
+        }
+
+        for (size_t i = 0; i < other.Length; i++)
+            newBuf[i] = other.m_buf[i];
+
+        delete[] m_buf;
+
+        m_buf      = newBuf;
+        m_capacity = other.m_capacity;
+        Length     = other.Length;
+        Error      = Utils::Error();
+    }
+
+    Buffer& operator=(Buffer&& other) noexcept
+    {
+        std::swap(m_buf, other.m_buf);
+        m_capacity = other.m_capacity;
+        Length     = other.Length;
+        Error      = other.Error;
+
+        return *this;
+    }
+
+    virtual ~Buffer()
+    {
+        delete[] m_buf;
+    }
+public:
+    void Realloc(size_t newLength)
+    {
+        if (Error)
+        {
+            Error.Print();
+            return;
+        }
+
+        if (newLength < m_capacity)
+            return;
+
+        size_t newCapacity = calculateCapacity(newLength);
+
+        T* newBuf = new T[newCapacity]{};
+
+        if (!newBuf)
+        {
+            Error = CREATE_ERROR(Utils::ErrorCode::ERROR_NO_MEMORY);
+            return;
+        }
+
+        for (size_t i = 0; i < Length; i++)
+            newBuf[i] = m_buf;
+
+        m_buf      = newBuf;
+        m_capacity = newCapacity;
+
+        delete[] m_buf;
+    }
+public:
+    char& operator[](size_t index) & noexcept
+    {
+        return m_buf[index];
+    }
+
+    const char& operator[](size_t index) const & noexcept
+    {
+        return m_buf[index];
+    }
+private:
+    static inline size_t calculateCapacity(size_t hintLength) noexcept
+    {
+        size_t capacity = DefaultCapacity;
+
+        while (capacity <= hintLength)
+            capacity *= GrowFactor;
+
+        return capacity;
+    }
+};
+
+} // namespace mlib
+
+#endif
--- headers/String.hpp
+++ headers/String.hpp
@@ -4,148 +4,16 @@
 #include <iostream>
 #include <cstring>
 #include "Utils.hpp"
+#include "GeneralBuffer.hpp"
 
 namespace mlib {
-
-template<size_t DefaultCapacity, size_t GrowFactor>
-class StringBuffer final
-{
-private:
-    char*  m_buf;
-    size_t m_capacity;
-public:
-    char*        RawPtr()       noexcept { return m_buf; }
-    const char*  RawPtr() const noexcept { return m_buf; }
-    size_t       Length;
-    Utils::Error Error = Utils::Error();
-private:
-    StringBuffer(size_t capacity, size_t length, const char* buf)
-        : m_buf(new char[capacity]{}), m_capacity(capacity), Length(length)
-    {
-        if (!m_buf)
-            Error = CREATE_ERROR(Utils::ErrorCode::ERROR_NO_MEMORY);
-        if (buf)
-            std::memcpy(m_buf, buf, length);
-    }
-public:
-    explicit StringBuffer(const char* buf = nullptr)
-        : StringBuffer(DefaultCapacity, 0, buf) {}
-
-    explicit StringBuffer(size_t hintLength, const char* buf = nullptr)
-        : StringBuffer(StringBuffer::calculateCapacity(hintLength), hintLength, buf) {}
-public:
-    StringBuffer(const StringBuffer& other)
-        : StringBuffer(other.m_capacity, other.Length)
-    {
-        if (Error) return;
-
-        std::memcpy(m_buf, other.m_buf, Length);
-    }
-
-    StringBuffer(StringBuffer&& other) noexcept
-        : m_buf(other.m_buf), m_capacity(other.m_capacity),
-          Length(other.Length)
-    {
-        other.m_buf = nullptr;
-    }
-
-    StringBuffer& operator=(const StringBuffer& other)
-    {
-        if (this == &other) return *this;
-
-        char* newBuf = new char[other.m_capacity]{};
-        if (!newBuf)
-        {
-            Error = CREATE_ERROR(Utils::ErrorCode::ERROR_NO_MEMORY);
-            return *this;
-        }
-
-        std::memcpy(newBuf, other.m_buf, other.Length);
-
-        delete[] m_buf;
-
-        m_buf      = newBuf;
-        m_capacity = other.m_capacity;
-        Length     = other.Length;
-        Error      = Utils::Error();
-    }
-
-    StringBuffer& operator=(StringBuffer&& other) noexcept
-    {
-        std::swap(m_buf, other.m_buf);
-        m_capacity = other.m_capacity;
-        Length     = other.Length;
-        Error      = other.Error;
-
-        return *this;
-    }
-
-    ~StringBuffer()
-    {
-        delete[] m_buf;
-    }
-public:
-    void Realloc(size_t newLength)
-    {
-        if (Error)
-        {
-            Error.Print();
-            return;
-        }
-
-        if (newLength < m_capacity)
-            return;
-
-        size_t newCapacity = calculateCapacity(newLength);
-
-        char* newBuf = new char[newCapacity]{};
-
-        if (!newBuf)
-        {
-            Error = CREATE_ERROR(Utils::ErrorCode::ERROR_NO_MEMORY);
-            return;
-        }
-
-        std::memcpy(newBuf, m_buf, Length);
-
-        m_buf      = newBuf;
-        m_capacity = newCapacity;
-
-        delete[] m_buf;
-    }
-public:
-    char& operator[](size_t index) & noexcept
-    {
-        return m_buf[index];
-    }
-
-    const char& operator[](size_t index) const & noexcept
-    {
-        return m_buf[index];
-    }
-
-    friend std::ostream& operator<<(std::ostream& out, const StringBuffer& buffer)
-    {
-        out << buffer.m_buf;
-        return out;
-    }
-private:
-    static inline size_t calculateCapacity(size_t hintLength) noexcept
-    {
-        size_t capacity = DefaultCapacity;
-
-        while (capacity <= hintLength)
-            capacity *= GrowFactor;
-
-        return capacity;
-    }
-};
 
 template<size_t DefaultCapacity = 8, size_t GrowFactor = 2>
 class String final
 {
 private:
-    StringBuffer<DefaultCapacity, GrowFactor> m_buf;
+    // StringBuffer<DefaultCapacity, GrowFactor> m_buf;
+    Buffer<char, DefaultCapacity, GrowFactor> m_buf;
 public:
     size_t       Length() { return m_buf.Length; }
     Utils::Error Error()  { return m_buf.Error;  }
@@ -195,7 +63,7 @@ public:
 
     friend std::ostream& operator<<(std::ostream& out, const String& string)
     {
-        return out << string.m_buf;
+        return out << string.m_buf.RawPtr();
     }
 };
 

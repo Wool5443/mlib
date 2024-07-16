7ae23fbe1caeab65af6d3d1929a914cd418a4bb2
Wool5443 <mihsolodilov2015@gmail.com>
2024-07-16 13:58:26 +0300

    Added Buffer documentation

--- headers/GeneralBuffer.hpp
+++ headers/GeneralBuffer.hpp
@@ -1,24 +1,58 @@
 #ifndef MLIB_GENERAL_BUFFER
 #define MLIB_GENERAL_BUFFER
 
+/**
+ * @file GeneralBuffer.hpp
+ * @author Misha Solodilov (mihsolodilov2015@gmail.com)
+ * @brief This file contains the implementation of a buffer
+ * used in other containers.
+ * @version 1.0
+ * @date 16-07-2024
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #include <iostream>
 #include <cstring>
 #include "Utils.hpp"
 
 namespace mlib {
 
+/**
+ * @brief Resizeable buffer allocated on heap
+ * 
+ * @tparam T value type
+ * @tparam DefaultCapacity
+ * @tparam GrowFactor
+ */
 template<typename T, std::size_t DefaultCapacity, std::size_t GrowFactor>
 class Buffer final
 {
 private:
-    T*     m_buf;
+    T*          m_buf;
     std::size_t m_capacity;
 public:
-    std::size_t       Length;
-    Utils::Error Error = Utils::Error();
+    std::size_t  Length; ///< Number of elements in the buffer
+    Utils::Error Error = Utils::Error(); ///< Buffer error
 public:
+    /**
+     * @brief returns the raw buffer
+     * 
+     * @return T* raw buffer
+     */
     inline T*            RawPtr()            noexcept { return m_buf;      }
+    /**
+     * @brief returns the raw buffer
+     * 
+     * @return T* raw buffer
+     */
     inline const T*      RawPtr()      const noexcept { return m_buf;      }
+    /**
+     * @brief Get the Capacity object
+     * 
+     * @return std::size_t 
+     */
     inline std::size_t   GetCapacity() const noexcept { return m_capacity; }
 private:
     Buffer(std::size_t capacity, std::size_t length, const char* buf)
@@ -35,12 +69,28 @@ private:
             std::memcpy((char*)m_buf, buf, length);
     }
 public:
-    explicit Buffer(const char* buf = nullptr)
-        : Buffer(DefaultCapacity, 0, buf) {}
-
+    /**
+     * @brief Construct a new Buffer object
+     */
+    explicit Buffer()
+        : Buffer(DefaultCapacity, 0, nullptr) {}
+    /**
+     * @brief Construct a new Buffer object
+     * 
+     * @param [in] hintLength length to ensure enough capacity
+     * to reduce reallocations
+     * @param [in] buf can be used for buffers
+     * of type without a ctor, because if passed
+     * it will std::memcpy buf. Also sets the length to hintLength
+     */
     explicit Buffer(std::size_t hintLength, const char* buf = nullptr)
         : Buffer(Buffer::calculateCapacity(hintLength), buf ? hintLength : 0, buf) {}
 public:
+    /**
+     * @brief Buffer copy constructor
+     * 
+     * @param [in] other buffer to copy
+     */
     Buffer(const Buffer& other)
         : Buffer(other.m_capacity, other.Length, nullptr)
     {
@@ -48,6 +98,11 @@ public:
             m_buf[i] = other[i];
     }
 
+    /**
+     * @brief Buffer move constructor
+     * 
+     * @param [in] other buffer to move
+     */
     Buffer(Buffer&& other) noexcept
         : m_buf(other.m_buf), m_capacity(other.m_capacity),
           Length(other.Length)
@@ -55,6 +110,12 @@ public:
         other.m_buf = nullptr;
     }
 
+    /**
+     * @brief Buffer copy assignment
+     * 
+     * @param [in] other buffer to copy
+     * @return Buffer&
+     */
     Buffer& operator=(const Buffer& other)
     {
         if (this == &other) return *this;
@@ -80,6 +141,12 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Buffer move assignment
+     * 
+     * @param [in] other buffer to move
+     * @return Buffer&
+     */
     Buffer& operator=(Buffer&& other) noexcept
     {
         std::swap(m_buf, other.m_buf);
@@ -95,13 +162,19 @@ public:
         delete[] m_buf;
     }
 public:
+    /**
+     * @brief Reallocates the buffer and changes its length
+     * 
+     * @param newLength
+     * @return Utils::Error 
+     */
     Utils::Error Realloc(std::size_t newLength)
     {
         if (Error)
             return Error;
 
         std::size_t oldLength = Length;
-        Length           = newLength;
+        Length                = newLength;
 
         if (newLength < m_capacity)
             return Utils::Error();

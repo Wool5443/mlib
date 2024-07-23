2728e1756c9631c06e8d3ef60d4552ca0ef0fec6
Wool5443 <mihsolodilov2015@gmail.com>
2024-07-23 11:37:39 +0300

    Prettier tests

--- example/main.cpp
+++ example/main.cpp
@@ -14,11 +14,21 @@ LOG_INIT_FILE(LOG_STREAM);
 
 int main()
 {
-    std::cout << "String:\n"    << GetErrorName(Tests::TestString(100)) << "\n\n";
-    std::cout << "Vector:\n"    << GetErrorName(Tests::TestVector(25))  << "\n\n";
-    std::cout << "List:\n"      << GetErrorName(Tests::TestList(25))    << "\n\n";
-    std::cout << "BTree:\n"     << GetErrorName(Tests::TestBTree())     << "\n\n";
-    std::cout << "HashTable:\n" << GetErrorName(Tests::TestHashTable()) << "\n\n";
+    const char* TEST_RESULTS[] =
+    {
+        GetErrorName(Tests::TestString(100)),
+        GetErrorName(Tests::TestVector(25)),
+        GetErrorName(Tests::TestList(25)),
+        GetErrorName(Tests::TestBTree()),
+        GetErrorName(Tests::TestHashTable()),
+    };
+    std::cout << "\n\n";
+
+    std::cout << "String:    " << TEST_RESULTS[0] << "\n";
+    std::cout << "Vector:    " << TEST_RESULTS[1] << "\n";
+    std::cout << "List:      " << TEST_RESULTS[2] << "\n";
+    std::cout << "BTree:     " << TEST_RESULTS[3] << "\n";
+    std::cout << "HashTable: " << TEST_RESULTS[4] << "\n";
 
     LOG("Hello, friends");
 
